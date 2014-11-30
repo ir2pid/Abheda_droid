5257b9ee7aeae88c609bc8a5d4c37ed3b826ceb6
ir2pid <ir2pid@gmail.com>
2014-12-01 00:31:17 +0530

    added gradel file
    gradel file with shared repos

--- app/src/main/java/com/noisyninja/abheda_droid/Constants.java
+++ app/src/main/java/com/noisyninja/abheda_droid/Constants.java
@@ -2,6 +2,10 @@ package com.noisyninja.abheda_droid;
 
 /**
  * Created by ir2pi on 11/30/2014.
+ *
+ * apache 2.0 libraries used:
+ * android-circlebutton https://github.com/markushi/android-circlebutton
+ * android-HoloCircularProgressBar https://github.com/passsy/android-HoloCircularProgressBar
  */
 public class Constants {
 
